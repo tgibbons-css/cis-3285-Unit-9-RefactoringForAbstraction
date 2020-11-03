0ab9e80b8f16af6965e4e8e9aaaca2773c1364d4
Tom Gibbons <tgibbons@css.edu>
2020-11-03 09:42:30 -0600

    Add project files.

---
+++ SingleResponsibilityPrinciple.AdoNet/AdoNetTradeStorage.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using SingleResponsibilityPrinciple.Contracts;
+
+namespace SingleResponsibilityPrinciple.AdoNet
+{
+    public class AdoNetTradeStorage : ITradeStorage
+    {
+        private readonly ILogger logger;
+
+        public AdoNetTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+
+            logger.LogInfo("INFO: Connecting to database");
+            // The first connection string uses |DataDirectory| 
+            //    and assumes the tradedatabase.mdf file is stored in 
+            //    SingleResponsibilityPrinciple\bin\Debug 
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;"))
+            // Template for connection string from database connection file
+            //    The @ sign allows for back slashes
+            //    Watch for double quotes which must be escaped using "" 
+            //    Watch for extra spaces after C: and avoid paths with - hyphens -
+            //    using (var connection = new System.Data.SqlClient.SqlConnection(@"  ;"))
+            //using (SqlConnection connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\tgibbons\source\repos\cis-3285-unit-8-f2020-tgibbons-css\Database\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    foreach (var trade in trades)
+                    {
+                        var command = connection.CreateCommand();
+                        command.Transaction = transaction;
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.CommandText = "dbo.insert_trade";
+                        command.Parameters.AddWithValue("@sourceCurrency", trade.SourceCurrency);
+                        command.Parameters.AddWithValue("@destinationCurrency", trade.DestinationCurrency);
+                        command.Parameters.AddWithValue("@lots", trade.Lots);
+                        command.Parameters.AddWithValue("@price", trade.Price);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }
+                connection.Close();
+            }
+
+            logger.LogInfo("{0} trades processed", trades.Count());
+        }
+    }
+}
